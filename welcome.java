9689eb86cbb11e361b3cfa31ec369135beac3ed2
neetishreddy <245122748006@mvsrec.edu.in>
2025-03-18 12:36:53 +0530

    Update welcome.java

--- welcome.java
+++ welcome.java
@@ -1,5 +1,5 @@
 class welcome{
-public static void main(string args[])
+public static void main(String args[])
 {
 System.out.println("welcome  to my project in devops");
 System.out.println("myself neetish reddy");

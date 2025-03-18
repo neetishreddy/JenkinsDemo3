8a1c10ac40caafb9ae08aee76463103096e3419b
neetishreddy <245122748006@mvsrec.edu.in>
2025-03-18 13:10:44 +0530

    Update py01.py

--- py01.py
+++ py01.py
@@ -1,55 +1,8 @@
-# This function adds two numbers
-def add(x, y):
-    return x + y
+# Take a list of numbers
+my_list = [12, 65, 54, 39, 102, 339, 221,]
 
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
+# use anonymous function to filter
+result = list(filter(lambda x: (x % 13 == 0), my_list))
 
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
-
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
-    else:
-        print("Invalid Input")
+# display the result
+print("Numbers divisible by 13 are",result)
